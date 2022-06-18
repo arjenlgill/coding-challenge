886d3ec817d2be45a69e25d400acee31cb42ae4a
arjenlgill <arjenlgill@gmail.com>
2022-06-18 11:59:42 +0100

    checkpoint #8 (sorting out couples)

--- submission.js
+++ submission.js
@@ -9,27 +9,36 @@ const names = [
 
 let people = [];
 
-class Person{
-    constructor(title, initial, first_name, last_name){
-
-        this.title = title ? title.toString() : null,
-        this.initial = initial ? initial.toString() : null,
-        this.first_name = first_name ? first_name.toString() : null,
-        this.last_name = last_name ? last_name.toString() : null // try to avoid repetition of ternary
-    }
+class Person {
+  constructor(title, initial, first_name, last_name) {
+    (this.title = title ? title.toString() : null),
+      (this.initial = initial ? initial.toString() : null),
+      (this.first_name = first_name ? first_name.toString() : null),
+      (this.last_name = last_name ? last_name.toString() : null); // try to avoid repetition of ternary
+  }
 }
 
 [...names].forEach((name) => {
+
+  let title, initial, first_name, last_name;
+
+  const assignCouple = () => {
+
     
-    let title = name.match(/^(?<!.)(dr)|(mr[s]?)|(miss)\.?/ig);
-     //only searching for singles for now.
-    let initial = name.match(/(?<= )\w\.?(?= )/ig);
+
+    name.match(/(and(?!\w+?)|&)/gi);
+  };
+
+  const assignSingle = () => {
+    title = name.match(/^(?<!.)(dr)|(mr[s]?)|(miss)\.?/gi);
+    //only searching for singles for now.
+    initial = name.match(/(?<= )\w\.?(?= )/gi);
     //just doing a loose search; tighten up later.
-    let first_name = name.match(/(?<= +?)(?!(and(?!\w)|&)|(dr)|(mr[s]?)|(miss))(\w(?!\.)){2,}(?= )/ig);
+    first_name = name.match(/(?<= +?)(?!(and(?!\w+?)|&)|(dr)|(mr[s]?)|(miss))(\w(?!\.)){2,}(?= )/gi);
     //making sure that surnames are at least 2 characters.
-    let last_name = name.match(/(?<= +?)\w+?$/ig);
+    last_name = name.match(/(?<= +?)\w+?$/gi);
     //making an assumption that the surname will absolutely be last in the string; this may be dangerous in larger datasets and/or where there is an unregulated input (i.e., genuine user values), but suffices within the scope of this challenge. same assumption applies to the titles (in that they will always be at the start).
+  };
 
-    console.log(new Person(title, initial, first_name, last_name));
-
-})
+  console.log(new Person(title, initial, first_name, last_name));
+});
