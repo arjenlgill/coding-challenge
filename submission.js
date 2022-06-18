af03861325aed47c9dee79a5931b1ce41a831780
arjenlgill <arjenlgill@gmail.com>
2022-06-18 10:08:52 +0100

    checkpoint #2 (planning and starting)

--- submission.js
+++ submission.js
@@ -8,3 +8,18 @@ const names = [
 ];
 
 let people = [];
+
+class Person{
+    constructor(title, initial, first_name, last_name){
+        this.title = title,
+        this.initial = initial,
+        this.first_name = first_name,
+        this.last_name = last_name,
+    }
+}
+
+[...names].forEach((name) => {
+    
+    let title = name.filter(filter => filter.match(/mr[\.]?/ig)
+
+})
