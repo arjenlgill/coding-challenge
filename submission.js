89491f3dfe7750a37683485c61b8e9cf3644e92b
arjenlgill <arjenlgill@gmail.com>
2022-06-18 11:38:50 +0100

    checkpoint #7

--- submission.js
+++ submission.js
@@ -15,7 +15,7 @@ class Person{
         this.title = title ? title.toString() : null,
         this.initial = initial ? initial.toString() : null,
         this.first_name = first_name ? first_name.toString() : null,
-        this.last_name = last_name ? last_name.toString() : null
+        this.last_name = last_name ? last_name.toString() : null // try to avoid repetition of ternary
     }
 }
 
@@ -25,12 +25,11 @@ class Person{
      //only searching for singles for now.
     let initial = name.match(/(?<= )\w\.?(?= )/ig);
     //just doing a loose search; tighten up later.
-    let first_name = name.match(/(?<= +?)(\w(?!\.)){2,}(?= )/ig);
+    let first_name = name.match(/(?<= +?)(?!(and(?!\w)|&)|(dr)|(mr[s]?)|(miss))(\w(?!\.)){2,}(?= )/ig);
     //making sure that surnames are at least 2 characters.
     let last_name = name.match(/(?<= +?)\w+?$/ig);
-    //making an assumption that the surname will absolutely be last in the string; this may be dangerous in larger datasets, but suffices within the scope of this challenge. same assumption applies to the titles (in that they will always be at the start).
+    //making an assumption that the surname will absolutely be last in the string; this may be dangerous in larger datasets and/or where there is an unregulated input (i.e., genuine user values), but suffices within the scope of this challenge. same assumption applies to the titles (in that they will always be at the start).
 
     console.log(new Person(title, initial, first_name, last_name));
-
 
 })
