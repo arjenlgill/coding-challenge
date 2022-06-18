8f9d9058a605344b1277a25fe54469b064a48f5e
arjenlgill <arjenlgill@gmail.com>
2022-06-18 11:15:14 +0100

    checkpoint #6

--- submission.js
+++ submission.js
@@ -1,5 +1,5 @@
 const names = [
-  "Mr. John Smith",
+  "Mr. A. John Smith",
   "Mr. G. Jones",
   "mr & mrs smith",
   "Dr and Mrs James Spade",
@@ -11,10 +11,11 @@ let people = [];
 
 class Person{
     constructor(title, initial, first_name, last_name){
-        this.title = title,
-        this.initial = initial,
-        this.first_name = first_name,
-        this.last_name = last_name
+
+        this.title = title ? title.toString() : null,
+        this.initial = initial ? initial.toString() : null,
+        this.first_name = first_name ? first_name.toString() : null,
+        this.last_name = last_name ? last_name.toString() : null
     }
 }
 
@@ -24,12 +25,12 @@ class Person{
      //only searching for singles for now.
     let initial = name.match(/(?<= )\w\.?(?= )/ig);
     //just doing a loose search; tighten up later.
-    let full_name = name.match(/(?<= +?)(\w(?!\.)){2,}(?= )/ig);
+    let first_name = name.match(/(?<= +?)(\w(?!\.)){2,}(?= )/ig);
     //making sure that surnames are at least 2 characters.
     let last_name = name.match(/(?<= +?)\w+?$/ig);
     //making an assumption that the surname will absolutely be last in the string; this may be dangerous in larger datasets, but suffices within the scope of this challenge. same assumption applies to the titles (in that they will always be at the start).
 
-    
+    console.log(new Person(title, initial, first_name, last_name));
 
 
 })
